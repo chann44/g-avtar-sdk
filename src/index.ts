074445c03e105ab178ec834e357ca23ebdecc72b
vikash <44chansw@gmail.com>
2022-09-21 13:17:57 +0530

    get random avtar

---
+++ src/index.ts
@@ -0,0 +1,18 @@
+import dbimages from "../db.json";
+
+let dbimg = JSON.stringify(dbimages);
+
+const parsedImages = JSON.parse(dbimg);
+
+function getRandomInt(min: number, max: number) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+export const getRandomAvtar = () => {
+  let img = parsedImages[getRandomInt(0, 1000)];
+  return img.url;
+};
+
+console.log(getRandomAvtar());
